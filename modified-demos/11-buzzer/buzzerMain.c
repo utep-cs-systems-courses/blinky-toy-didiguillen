5bf721b4c775db80f1a16ded43d831b39f5dcbd7
didiguillen <daguillen@miners.utep.edu>
2023-07-25 20:44:21 -0600

    testing buttons in dir 9 and starting buzzer in 11

--- modified-demos/11-buzzer/buzzerMain.c
+++ modified-demos/11-buzzer/buzzerMain.c
@@ -6,9 +6,31 @@ int main() {
     configureClocks();
  
     buzzer_init();
-    buzzer_set_period(8581);	/* start buzzing!!! 2MHz/1000 = 2kHz*/
+    //	/* start buzzing!!! 2MHz/1000 = 2kHz*/
     //Buddy Holly notes:
     //9631, 5727, 6428, 7645, 9631, 8581, 7645, 8581, 9631, 11453, 12857  
-
+    enableWDTInterrupts();
     or_sr(0x18);          // CPU off, GIE on
+}
+int quarterNoteLength = 125;
+int eighthNoteLength = 63;
+int seconds = 0;
+int notes[]= {9361, 5727, 6428, 7645, 9631, 8581, 7645, 8581, 9631, 11453, 12857};
+int noteLength[] = {1, 1, 1, 0, 0, 0, 0, 0, 0, 1, 1};
+int i = 0;
+
+void __interrupt_vec(WDT_VECTOR) WDT()
+{
+  if(i = 0){
+    buzzer_set_period(9361);
+  }
+  seconds++;
+  if(i > 10)
+    i = 0;
+  if(noteLength[i] >= seconds){
+    seconds = 0;
+    i++;
+    buzzer_set_period(notes[i]);
+  }
+  
 }
--- modified-demos/9-switch-flash-combo/main.c
+++ modified-demos/9-switch-flash-combo/main.c
@@ -6,18 +6,34 @@
 #define LEDS (LED_RED | LED_GREEN)
 
 #define SW1 BIT3		/* switch1 is p1.3 */
-#define SWITCHES SW1		/* only 1 switch on this board */
+#define SWITCHBOT SW1		/* only 1 switch on this board */
+#define SW2 BIT0
+#define SWITCHONE SW2
+#define SW3 BIT1
+#define SWITCHTWO SW3
+#define SW4 BIT2
+#define SWITCHTHREE SW4
+#define SW5 BIT3
+#define SWITCHFOUR SW5
+#define SWITCHTOP (SWITCHONE | SWITCHTWO | SWITCHTHREE | SWITCHFOUR)
 
 #define BLINK1 0
 #define BLINK2 1
 #define BLINK3 2
-void blink_state(void);
+#define BLINK4 3
+#define BLINK5 5
+void blink_state(int);
+void switch_interrupt_handler_2();
 
 void switch_init() {
-  P1REN |= SWITCHES;		/* enables resistors for switches */
-  P1IE |= SWITCHES;		/* enable interrupts from switches */
-  P1OUT |= SWITCHES;		/* pull-ups for switches */
-  P1DIR &= ~SWITCHES;		/* set switches' bits for input */
+  P1REN |= SWITCHBOT;		/* enables resistors for switches */
+  P1IE |= SWITCHBOT;		/* enable interrupts from switches */
+  P1OUT |= SWITCHBOT;		/* pull-ups for switches */
+  P1DIR &= ~SWITCHBOT;		/* set switches' bits for input */
+  P2REN |= SWITCHTOP;
+  P2IE |= SWITCHTOP;
+  P2OUT |= SWITCHTOP;
+  P2DIR &= ~SWITCHTOP;
 }
 
 void led_init() {
@@ -47,8 +63,8 @@ switch_interrupt_handler()
   char p1val = P1IN;		/* switch is in P1 */
 
 /* update switch interrupt sense to detect changes from current buttons */
-  P1IES |= (p1val & SWITCHES);	/* if switch up, sense down */
-  P1IES &= (p1val | ~SWITCHES);	/* if switch down, sense up */
+  P1IES |= (p1val & SWITCHBOT);	/* if switch up, sense down */
+  P1IES &= (p1val | ~SWITCHBOT);	/* if switch down, sense up */
 
   if (p1val & SW1) {		/* button up */
     P1OUT &= ~LED_GREEN;
@@ -58,32 +74,72 @@ switch_interrupt_handler()
     buttonDown = 1;
   }
 }
+void switch_interrupt_handler_2(){
+  char p2val = P2IN;
+  P2IES |= (p2val & SWITCHTOP);
+  P2IES &= (p2val | ~SWITCHTOP);
 
-int countpress = 0;
+  if(p2val & SW2){
+    P1OUT &= ~LED_GREEN;
+    buttonDown = 0;
+  }
+  else {
+    P1OUT |= LED_GREEN;
+    buttonDown = 1;
+  }
+  
+}
+
 char state = BLINK1;
 
-void blink_state(void)
+void blink_state(int num)
 {
-  switch(countpress){
+  switch(num){
   case 1:
     state = BLINK1;
     break;
   case 2:
     state = BLINK2;
     break;
-  default:
+  case 3:
     state = BLINK3;
-    countpress = 0;
+    break;
+  case 4:
+    state = BLINK4;
+    break;
+  default:
+    state = BLINK5;
   }
 }
 /* Switch on P1 (S2) */
 void
 __interrupt_vec(PORT1_VECTOR) Port_1(){
-  if (P1IFG & SWITCHES) {	      /* did a button cause this interrupt? */
-    P1IFG &= ~SWITCHES;		      /* clear pending sw interrupts */
-    countpress++;
-    blink_state();
+  if (P1IFG & SWITCHBOT) {	      /* did a button cause this interrupt? */
+    P1IFG &= ~SWITCHBOT;		      /* clear pending sw interrupts */
+    //countpress++;
+    blink_state(0);
     switch_interrupt_handler();	/* single handler for all switches */
+  }
+}
+
+void
+__interrupt_vec(PORT2_VECTOR) Port_2(){
+  if (P2IFG & SWITCHONE){
+    P2IFG &= ~SWITCHONE;
+    blink_state(1);
+    switch_interrupt_handler_2();
+  }
+  else if (P2IFG & SWITCHTWO){
+    P2IFG &= ~SWITCHTWO;
+    blink_state(2);
+  }
+  else if (P2IFG & SWITCHTHREE){
+    P2IFG &= ~SWITCHTHREE;
+    blink_state(3);
+  }
+  else if(P2IFG & SWITCHFOUR){
+    P2IFG &= ~SWITCHFOUR;
+    blink_state(4);
   }
 }
 
@@ -113,9 +169,31 @@ __interrupt_vec(WDT_VECTOR) WDT()	/* 250 interrupts/sec */
       if(!buttonDown) P1OUT &= ~LED_RED;
     }
   }
+  else if(state == BLINK3){
+    switch(blink_count){
+    case 18:
+      blink_count = 0;
+      P1OUT |= LED_RED;
+      break;
+    default:
+      blink_count++;
+      if(!buttonDown) P1OUT &= ~LED_RED;
+    }
+  }
+  else if(state == BLINK4){
+    switch(blink_count){
+    case 24:
+      blink_count = 0;
+      P1OUT |= LED_RED;
+      break;
+    default:
+      blink_count++;
+      if(!buttonDown) P1OUT &= ~LED_RED;
+    }
+  }
   else{ //doesn't work, ask at coaching session
     switch(blink_count){
-    case 24: 
+    case 30: 
       blink_count = 0;
       P1OUT |= LED_RED;
       break;
